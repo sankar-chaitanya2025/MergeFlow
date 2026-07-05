929301dbc7251edfbb68f4c1eee54e87d11740fa
sankar-chaitanya2025 <sankar.chaitanya2025@gmail.com>
2026-07-05 14:08:21 +0530

    feat(auth): configure Auth.js with GitHub provider and env vars

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,8 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
+
+import { env } from "~/env";
 
 import { db } from "~/server/db";
 import {
@@ -38,7 +40,10 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    GitHubProvider({
+      clientId: env.AUTH_GITHUB_ID,
+      clientSecret: env.AUTH_GITHUB_SECRET,
+    }),
     /**
      * ...add more providers here.
      *
